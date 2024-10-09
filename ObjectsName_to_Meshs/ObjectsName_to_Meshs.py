a74c21f8b311ccf6c148bbce9fdaffd02b281069
moteki <49066959+a2d4f3s1@users.noreply.github.com>
2024-10-10 01:02:41 +0900

    Add files via upload

--- ObjectsName_to_Meshs/ObjectsName_to_Meshs.py
+++ ObjectsName_to_Meshs/ObjectsName_to_Meshs.py
@@ -1,1 +1,21 @@
+## メッシュ名をオブジェクト名に変更し、メッシュリンクなオブジェクトを選択
+
 import bpy
+objects = bpy.data.objects
+shareObjects = list()
+
+## Deselect All
+for object in objects:
+    bpy.context.scene.objects[(object.name)].select_set(False)
+
+## Copy Name obj to mesh
+for obj in objects:
+    if obj.data and obj.data.users == 1: ## if no shared mesh
+        obj.data.name = obj.name
+    else :
+        print (obj)
+        shareObjects.append(obj)
+
+## Select Linked mesh obj
+for obj in shareObjects:
+    bpy.context.scene.objects[(obj.name)].select_set(True)
